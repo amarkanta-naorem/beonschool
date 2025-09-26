a100706b7284abff4e450750226e7f340e4697ce
amarkanta-naorem <amarkanta.meitei.naorem@gmail.com>
2025-09-26 23:43:35 +0530

    react-icons

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -41,7 +41,7 @@ export default function Navbar() {
       <div className={`md:hidden fixed top-0 right-0 h-full w-64 bg-gray-950 border-l border-gray-800 transform ${isOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-300 ease-in-out z-50`}>
         <div className="px-6 py-4">
           {isOpen && (
-            <button onClick={() => setIsOpen(false)} className="absolute top-0 -left-8.5 z-60">
+            <button onClick={() => setIsOpen(false)} className="absolute top-3 -left-8.5 z-60">
               <div className="relative bg-gray-900/95 backdrop-blur-sm p-1 rounded-tl-xl rounded-bl-xl shadow-2xl border border-gray-600/30 transform transition-transform duration-300 hover:scale-105">
                 <FiX size={24} color="white" className="filter drop-shadow"/>
                 <div className="absolute inset-0 rounded-tl-xl rounded-bl-xl shadow-2xl bg-gradient-to-r from-indigo-500/10 to-purple-500/10" />
@@ -49,7 +49,7 @@ export default function Navbar() {
             </button>
           )}
 
-          <nav className="mt-8">
+          <nav className="">
             <ul className="flex flex-col space-y-6">
               <li>
                 <Link href="/courses" className="text-white font-semibold text-lg hover:text-indigo-300 transition-colors" onClick={() => setIsOpen(false)}>Courses</Link>
